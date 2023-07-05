cbd48e35c3be4fc3162496d81f0cd38a4e78cf09
m4nkej <mankej91@yahoo.pl>
2023-07-05 12:41:39 +0200

    unknow never

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult1(num) {
-    console.log("Result " + num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-printResult1(add(2, 5));
-function addAndHandle(a, b, cb) {
-    var result = a + b;
-    cb(result);
-}
-var combineValues;
-combineValues = add;
-//combineValues = printResult1;
-console.log(combineValues(50, 4));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError("An error", 500);
+console.log(result);
