18e22e8b07491cff0d90589bb21b1e881b305a75
m4nkej <mankej91@yahoo.pl>
2023-07-05 11:14:43 +0200

    array

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
 var person = {
     name: "Test",
     age: 30,
+    hobbies: ["sports", "cooking"],
 };
-console.log(person);
+console.log(person.name);
+var favoritesActivities;
+favoritesActivities = ["Sports"];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -5,5 +5,13 @@
 const person = {
   name: "Test",
   age: 30,
+  hobbies: ["sports", "cooking"],
 };
 console.log(person.name);
+
+let favoritesActivities: string[];
+favoritesActivities = ["Sports"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
