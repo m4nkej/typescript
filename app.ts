179f29b9c65935dd1d2b148a0ada5235010f38dd
m4nkej <mankej91@yahoo.pl>
2023-07-05 10:58:41 +0200

    object

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  let result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
-
-const number1 = 5;
-const number2 = "2.8";
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-add(+number1, +number2, printResult, resultPhrase);
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "Test",
+  age: 30,
+};
+console.log(person.name);
