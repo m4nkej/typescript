77363e7c456b1fffdf318d46284e441c5249ef06
m4nkej <mankej91@yahoo.pl>
2023-07-05 11:37:21 +0200

    enum

--- app.ts
+++ app.ts
@@ -1,14 +1,27 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
-  //const person = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   //const person = {
+//   name: "Test",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 7,
+  AUTHOR,
+}
+
+const person = {
   name: "Test",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 // person.role.push("admin");
@@ -23,4 +36,8 @@ favoritesActivities = ["Sports"];
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
