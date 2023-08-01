5a59175b63519bbf8ec7c7f77f691877fab44a17
m4nkej <mankej91@yahoo.pl>
2023-08-01 10:20:45 +0200

    add webpack for prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+  entry: "./src/app.ts",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: false,
+  module: {
+    rules: [{ test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ }],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new CleanWebpackPlugin.CleanWebpackPlugin()],
+};
